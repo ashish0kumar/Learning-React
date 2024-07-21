4b1ee8e6745b37f77f7d7d506d739d9f070ebfef
Ashish Kumar <ashishkumar901336@gmail.com>
2024-07-22 00:32:02 +0530

    applying nested rules using styled components

--- 02-Styling/src/components/AuthInputs.jsx
+++ 02-Styling/src/components/AuthInputs.jsx
@@ -6,7 +6,7 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
+`;
 
 const Label = styled.label`
   display: block;
@@ -16,7 +16,7 @@ const Label = styled.label`
   letter-spacing: 0.1em;
   text-transform: uppercase;
   color: ${({ $invalid }) => $invalid ? "#f87171" : "#6b7280"};
-`
+`;
 
 const Input = styled.input`
   width: 100%;
@@ -27,7 +27,7 @@ const Input = styled.input`
   border: 1px solid ${({ $invalid }) => $invalid ? "#f73f3f" : "transparent"};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
