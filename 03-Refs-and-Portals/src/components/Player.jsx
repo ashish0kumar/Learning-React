4a56d2262732cbcf4d0cb88a6d48cef3c72b9bf2
Ashish Kumar <ashishkumar901336@gmail.com>
2024-07-24 18:03:54 +0530

    adding challenges component

--- 03-Refs-and-Portals/src/components/Player.jsx
+++ 03-Refs-and-Portals/src/components/Player.jsx
@@ -6,7 +6,7 @@ export default function Player() {
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
-    playerName.current.value = "";
+    playerName.current.value = ""; // imperative code
   }
 
   return (
