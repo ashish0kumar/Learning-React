2a3d0a60d397c26f7f9e1a14ee1e2e1d88fd42b8
Ashish Kumar <ashishkumar901336@gmail.com>
2024-08-25 01:03:56 +0530

    optimizing state updates by moving the progress bar in a diff component so that only the bar is rerendered continously

--- 05-Side-Effects/src/components/DeleteConfirmation.jsx
+++ 05-Side-Effects/src/components/DeleteConfirmation.jsx
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
 
 const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
   useEffect(() => {
-    const startTime = Date.now();
-
-    const interval = setInterval(() => {
-      const elapsedTime = Date.now() - startTime;
-      const newRemainingTime = Math.max(TIMER - elapsedTime, 0);
-      setRemainingTime(newRemainingTime);
-
-      if (newRemainingTime <= 0) {
-        clearInterval(interval);
-        onConfirm();
-      }
-    }, 10);
+    console.log("TIMER SET");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
 
     return () => {
-      clearInterval(interval);
-    };
+      console.log("CLEANING UP TIMER");
+      clearTimeout(timer);
+    }
   }, [onConfirm]);
 
   return (
@@ -36,7 +28,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} onConfirm={onConfirm} />
     </div>
   );
 }
